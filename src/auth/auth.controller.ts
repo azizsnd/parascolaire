8baadf98e466daf98569546b8c4738f7ff67b4d6
Aziz Sandid <sandid.aziz123@gmail.com>
2024-12-24 00:20:49 +0100

    password-mai

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body, Request, UseGuards } from '@nestjs/common';
+import { Controller, Post, Request, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LocalGuard } from 'src/common/guards/local.guard';
 import { Public } from 'src/common/decorators/public.decorator';
@@ -6,7 +6,7 @@ import { Public } from 'src/common/decorators/public.decorator';
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
-  
+
   @Post('login')
   @Public()
   @UseGuards(LocalGuard)
